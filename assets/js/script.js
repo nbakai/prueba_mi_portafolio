4c318ef84faaaca3b111f87d62810a887b5082b0
Melisa Budini <melixbq@gmail.com>
2020-08-17 15:22:47 -0400

    carousel agregado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+$(function(){
+    $('a').click(function(event){
+        if(this.hash !== "") {
+            event.preventDefault();
+            var x = this.hash;
+            $("html, body").animate({
+                scrollTop: $(x).offset().top
+            }, 800, function(){
+                window.location.hash = x;
+            });
+        }
+    });
+});
+
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   });
